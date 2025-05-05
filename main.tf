60f8d069fe2fd40b9807b93111c1a7caff8cd56c
kolosovpetro <kolosovp94@gmail.com>
2025-05-05 15:17:04 +0200

    deploy windows paas update

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ resource "azurerm_resource_group" "public" {
 }
 
 ##########################################################################
-# APP SERVICE
+# APP SERVICE WINDOWS
 ##########################################################################
 
-resource "azurerm_service_plan" "asp" {
-  name                = "asp-prod-${var.prefix}"
+resource "azurerm_service_plan" "windows" {
+  name                = "asp-windows-${var.prefix}"
   location            = azurerm_resource_group.public.location
   resource_group_name = azurerm_resource_group.public.name
 
@@ -21,10 +21,37 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_windows_web_app" "app" {
-  name                = "ase-prod-${var.prefix}"
+  name                = "ase-windows-${var.prefix}"
   location            = azurerm_resource_group.public.location
   resource_group_name = azurerm_resource_group.public.name
-  service_plan_id     = azurerm_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.windows.id
+
+  app_settings = {
+    WEBSITE_ENABLE_SYNC_UPDATE_SITE = "true"
+    WEBSITE_RUN_FROM_PACKAGE        = "1"
+  }
+
+  site_config {}
+}
+
+##########################################################################
+# APP SERVICE LINUX
+##########################################################################
+
+resource "azurerm_service_plan" "linux" {
+  name                = "asp-linux-${var.prefix}"
+  location            = azurerm_resource_group.public.location
+  resource_group_name = azurerm_resource_group.public.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "ase-linux-${var.prefix}"
+  location            = azurerm_resource_group.public.location
+  resource_group_name = azurerm_resource_group.public.name
+  service_plan_id     = azurerm_service_plan.linux.id
 
   app_settings = {
     WEBSITE_ENABLE_SYNC_UPDATE_SITE = "true"
